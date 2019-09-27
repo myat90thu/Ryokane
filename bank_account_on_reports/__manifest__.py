0dd0f5b72f71ec936f9c1bb5995651004f68342d
Malek Bakri <malek.bakri@hloul-bas.com>
2019-09-27 14:14:42 +0300

    adding bank account to sales order and invoices

---
+++ bank_account_on_reports/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    "name": "Add bank account on reports",
+    "version": "12.0.1.0.0",
+    "author": "BAS",
+    "summary": "Add bank account on reports",
+    'license': 'AGPL-3',
+    'category': 'Accounting',
+    "depends":
+        ['sale_management','account'],
+ 
+    'data': [
+    'views/views.xml',
+    ],
+    'demo': [
+       
+    ],
+    'installable': True,
+}
