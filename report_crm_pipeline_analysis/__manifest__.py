05c634369a928e7c9da2b3e01ac27ed0869522af
Malek Bakri <malek.bakri@hloul-bas.com>
2019-10-07 18:16:35 +0300

    Adding pipeline analysis module

---
+++ report_crm_pipeline_analysis/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Pipeline Analysis by closing date',
+    'version': '12.0.1.0',
+    'sequence': 1,
+    'category': 'CRM',
+    'description':'',
+    'depends': ['base','crm'],
+    'data': [
+        'views/crm_pipeline_report_views.xml'
+        ],
+    'installable': True,
+    'auto_install': False,
+    
+
+
+}
