b7be2fc1a91dfe1555d40738f53de3c62d6946b8
Malek Bakri <malek.bakri@hloul-bas.com>
2019-10-09 16:51:58 +0300

    adding access for all users to reps

---
+++ board_access_for_all/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'BAS DASHBOARDS',
+    'version': '1.0',
+    'category': 'Extra Tools',
+    'summary': 'Build your own dashboards',
+    'description': """
+Lets the user create a custom dashboard.
+========================================
+
+Allows users to create custom dashboard.
+    """,
+    'depends': ['base', 'web'],
+    'data': [
+    ],
+    'installable': True,
+}
