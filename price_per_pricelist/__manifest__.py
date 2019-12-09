0fa5e576a5fc06fab19c5409c03f5367388f398a
Malek Bakri <malek.bakri@hloul-bas.com>
2019-12-09 16:01:23 +0200

    add pricelist piv

---
+++ price_per_pricelist/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2017 Carlos Dauden <carlos.dauden@tecnativa.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Product Pricelist Print",
+    "summary": "Print price list from menu option, product templates, "
+               "products variants or price lists",
+    "version": "12.0.1.0.0",
+    "category": "Product",
+    "website": "",
+    "author":
+        "BAS",
+    "license": "AGPL-3",
+    "depends": [
+        "sale",
+    ],
+    "data": [
+        "views/report_product_pricelist.xml",
+    ],
+}
