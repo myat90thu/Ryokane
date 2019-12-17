0822a2545e30b5145f7706772aa72342d199b30d
Malek Bakri <malek.bakri@hloul-bas.com>
2019-12-17 11:24:59 +0200

    adding pricelist items

--- price_per_pricelist/__manifest__.py
+++ price_per_pricelist/__manifest__.py
@@ -1,9 +1,8 @@
 # Copyright 2017 Carlos Dauden <carlos.dauden@tecnativa.com>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
-    "name": "Product Pricelist Print",
-    "summary": "Print price list from menu option, product templates, "
-               "products variants or price lists",
+    "name": "Print Pricelist Item",
+    "summary": "Print pricelist Items",
     "version": "12.0.1.0.0",
     "category": "Product",
     "website": "",
