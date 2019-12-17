967f55057e0603fa33176dabbc2d9178e12d7313
Malek Bakri <malek.bakri@hloul-bas.com>
2019-12-17 13:02:15 +0200

    salestarget

---
+++ sale_target/__manifest__.py
@@ -0,0 +1,23 @@
+
+{
+    'name': 'Sales Team Target Management',
+    'version': '1.1',
+    'summary': 'Sales Team Target Management',
+    'author': 'BAS',
+    'category': 'Sales',
+    'website': 'http://www.bas.sarl',
+    'description': '',
+    'depends': [
+        'base',
+        'sale',
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/sale_target_view.xml',
+    ],
+    'images': [''
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
---
+++ sale_target/models/sales_person_target.py
@@ -0,0 +1,44 @@
+from odoo import api, fields, models
+
+
+class SalesTarget(models.Model):
+    _name = 'sale.target'
+
+    name = fields.Char(string='Reference')
+    active = fields.Boolean(string="Active", default=True, track_visibility='onchange')
+    from_date = fields.Date(string="From Date")
+    end_date = fields.Date(string="To Date")
+    user_line = fields.One2many('sales.person', 'line_id', string="Salesperson List")
+
+    @api.multi
+    def sales(self):
+        sales = []
+        for line in self.user_line:
+            if line:
+                line.actual = 0
+                line.pending = 0
+        saleorder = self.env['sale.order']
+        for line in self.user_line:
+            sales = saleorder.search([
+                ('user_id', '=', line.user_id.id),
+                ('state', '=', 'sale'),
+                ('confirmation_date', '>=', self.from_date),
+                ('confirmation_date', '<=', self.end_date)
+            ])
+            if sales:
+                for sale in sales:
+                    for line in self.user_line:
+                        if line.user_id == sale.user_id:
+                            line.actual += sale.amount_total
+                            line.pending = line.target - sale.amount_total
+        return True
+
+
+class SalesPerson(models.Model):
+    _name = 'sales.person'
+
+    user_id = fields.Many2one('res.users', string="Sales Person")
+    target = fields.Float(string="Target")
+    actual = fields.Float(string="Actual")
+    pending = fields.Float(string="Pending")
+    line_id = fields.Many2one('sale.target', string="Target Id")
