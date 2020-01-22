15867fb51010977660c7f1add398ec0986d417f2
Malek Bakri <malek.bakri@hloul-bas.com>
2020-01-22 14:01:40 +0200

    adding tabs to products

---
+++ additional_product_template/__manifest__.py
@@ -0,0 +1,16 @@
+{
+  "name"                 :  "Additional Product Tab",
+  "summary"              :  "Adding additional product customization in website",
+  "category"             :  "Website",
+  "version"              :  "12.0.1",
+  "author"               :  "BAS Sarl",
+  "website"              :  "https://bas.sarl",
+  "depends"              :  ['website_sale'],
+  "data"                 :  [
+                             'views/template.xml',
+                             'views/views.xml',
+                             'security/ir.model.access.csv',
+                            ],
+  "application": True,
+  "installable":  True,
+}
