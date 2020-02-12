e3dc649eeccfd359c15ecf864285d32b844167ab
Malek Bakri <malek.bakri@hloul-bas.com>
2020-02-12 13:08:03 +0200

    3 cols per row

---
+++ custom_number_of_product_cols/__manifest__.py
@@ -0,0 +1,12 @@
+{
+  "name"                 :  "3 Product per Column",
+  "summary"              :  "",
+  "category"             :  "Website",
+  "version"              :  "12.0.1",
+  "author"               :  "BAS Sarl",
+  "website"              :  "https://bas.sarl",
+  "depends"              :  ['website_sale'],
+  "data"                 :  [
+                            ],
+  "installable":  True,
+}
---
+++ custom_number_of_product_cols/controllers/main.py
@@ -0,0 +1,3 @@
+from odoo.addons.website_sale.controllers import main
+main.PPG = 18 # Products per Page
+main.PPR = 3 # Products per Row
